a849ee50b7724ee1aaedbb7161114ba45c35020a
YuhaiZhang <47788401+yuhaizhang@users.noreply.github.com>
2019-07-24 05:27:55 +0800

    Add some test function
    添加一个Fn键功能，实现系统声音控制
    添加一个屏蔽WIN键功能的切换按钮，并关联了一个pin（C3）用于连接LED来显示屏蔽状态（屏蔽时置为高电位，允许时置为低电位）
    添加系统NUM_LOCK、CAPS_LOCK、SCROLL_LOCK灯状态的监控，并关联pin（分别为C0、C1、C2）用于连接LED来显示对应状态

--- keyboards/dzkb/keymaps/default/keymap.c
+++ keyboards/dzkb/keymaps/default/keymap.c
@@ -15,6 +15,10 @@
  */
 #include QMK_KEYBOARD_H
 
+#define _BL 0
+#define _FL 1
+#define _WIN_DISABLE 2
+
 // Defines the keycodes used by our macros in process_record_user
 enum custom_keycodes {
   QMKBEST = SAFE_RANGE,
@@ -22,9 +26,17 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT( /* Base */
+  [_BL] = LAYOUT( /* Base */
     KC_A,  KC_1,  KC_H, \
-      KC_TAB,  KC_SPC   \
+    MO(_FL),  TG(_WIN_DISABLE),  KC_LGUI   \
+  ),
+  [_FL] = LAYOUT( /* Fn */
+    KC_AUDIO_MUTE,  KC_AUDIO_VOL_DOWN,  KC_AUDIO_VOL_UP, \
+    KC_TRNS,  KC_TRNS,  KC_LGUI   \
+  ),
+  [_WIN_DISABLE] = LAYOUT( /* Win_Disable */
+    KC_TRNS,  KC_TRNS,  KC_TRNS, \
+    KC_TRNS,  KC_TRNS,  KC_NO   \
   ),
 };
 
@@ -66,6 +78,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+uint32_t layer_state_set_user(uint32_t state) {
+    switch (biton32(state)) {
+    case _WIN_DISABLE:
+        writePinHigh(C3);
+        break;
+    default: //  for any other layers, or the default layer
+        writePinLow(C3);
+        break;
+    }
+  return state;
+}
+
 void matrix_init_user(void) {
 
 }
@@ -75,5 +99,19 @@ void matrix_scan_user(void) {
 }
 
 void led_set_user(uint8_t usb_led) {
-
+    if (IS_LED_ON(usb_led, USB_LED_NUM_LOCK)) {
+        writePinHigh(C0);
+    } else {
+        writePinLow(C0);
+    }
+    if (IS_LED_ON(usb_led, USB_LED_CAPS_LOCK)) {
+        writePinHigh(C1);
+    } else {
+        writePinLow(C1);
+    }
+    if (IS_LED_ON(usb_led, USB_LED_SCROLL_LOCK)) {
+        writePinHigh(C2);
+    } else {
+        writePinLow(C2);
+    }
 }
